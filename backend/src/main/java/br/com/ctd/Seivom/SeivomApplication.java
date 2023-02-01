0f0a9b2ab5ed5c774373765f523647b261ba40cc
davimc <davimatosc@hotmail.com>
2023-02-01 15:13:33 -0300

    Created

---
+++ backend/src/main/java/br/com/ctd/Seivom/SeivomApplication.java
@@ -0,0 +1,13 @@
+package br.com.ctd.Seivom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeivomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeivomApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/ctd/Seivom/SeivomApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ctd.Seivom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeivomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
