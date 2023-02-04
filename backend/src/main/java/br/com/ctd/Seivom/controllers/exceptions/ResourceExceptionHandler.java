225feeb886e677c4e5f54cb66f28a09c7058fd59
davimc <davimatosc@hotmail.com>
2023-02-04 07:32:07 -0300

    Exception handling

---
+++ backend/src/main/java/br/com/ctd/Seivom/controllers/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,26 @@
+package br.com.ctd.Seivom.controllers.exceptions;
+
+import br.com.ctd.Seivom.services.exceptions.ObjectNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError();
+        err.setTimeStamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Resource not found");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+    }
+}
